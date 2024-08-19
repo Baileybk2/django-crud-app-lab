d9f8e8749f8fd93b7a2ce1b999e09c2e9167c877
BaileyBeck <baileybk2@gmail.com>
2024-08-19 17:40:24 -0400

    added bug detail page and created superuser

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bug
 
+admin.site.register(Bug)
 # Register your models here.
--- my_app/views.py
+++ my_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-class Bug:
-    def __init__(self, name, species, description, lifespan):
-        self.name = name
-        self.species = species
-        self.description = description
-        self.lifespan = lifespan 
-
-bugs = [
-    Bug('June Bug', 'Cotinis nitida', 'Shiny, green beetle', 3),
-    Bug('Praying Mantis', 'Mantis religiosa', 'Triangular head with large eyes; long, pointed arms',1),
-    Bug('Jumping Spider', 'Salticidae', 'Small, fuzzy, adorable; loves to dance', 2),
-    Bug('Dragonfly', 'Anisoptera', 'Long body with large eyes; high aspect ratio wings', 1),
-]
+from .models import Bug
 
 # Create your views here.
 def home(request):
@@ -23,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def bug_index(request):
+    bugs = Bug.objects.all()
     return render(request, 'bugs/index.html', {'bugs': bugs })
+
+def bug_detail(request, bug_id):
+    bug = Bug.objects.get(id=bug_id)
+    return render(request, 'bugs/detail.html', {'bug': bug})
