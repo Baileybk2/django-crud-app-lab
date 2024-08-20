da7fc33fa8e0a7a6b85ac1fcbfe9b3bf2c2eb1cf
BaileyBeck <baileybk2@gmail.com>
2024-08-20 17:08:45 -0400

    added sighting model

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bug
+from .models import Bug, Sighting
 
 admin.site.register(Bug)
 # Register your models here.
+admin.site.register(Sighting)
--- my_app/models.py
+++ my_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.urls import reverse 
 
 # Create your models here.
+
+TIME = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening'),
+    ('N', 'Night')
+)
+
 class Bug(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -13,3 +21,17 @@ class Bug(models.Model):
     
     def get_absolute_url(self):
         return reverse('bug-detail', kwargs={'bug_id': self.id})
+    
+
+class Sighting(models.Model):
+    date = models.DateField('Sighting Date')
+    time = models.CharField(
+        max_length=1, 
+        choices=TIME, 
+        default=TIME[0][0]
+    )
+
+    bug = models.ForeignKey(Bug, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
