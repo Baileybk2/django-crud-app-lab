ee112931e2bcc66395f386664bc0d1ef76c152b7
BaileyBeck <baileybk2@gmail.com>
2024-08-20 18:40:08 -0400

    added login, logout, and navbar updates

--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from .models import Bug
 from .forms import SightingForm
 
+
+
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+class Home(LoginView):
+    template_name = 'home.html'
 
 def about(request): 
     return render(request, 'about.html')
